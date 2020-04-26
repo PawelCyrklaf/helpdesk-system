6709386804067865af30850da4c05145d82b30e0
Paweł Cyrklaf <pawel.cyrklaf@gmail.com>
2020-04-25 22:01:15 -0400

    Add maker bundle for dev

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
