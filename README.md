8a6bca4f16eda0a99f6aacf6cee6694509523745
Paweł Cyrklaf <pawel.cyrklaf@gmail.com>
2020-04-25 21:42:32 -0400

    Configuration commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# HelpDesk System API
+
+Symfony 4 RESTful API for helpdesk system
+
+## Backend technology
+1. Symfony 4.4
+1. PHP 7.4 on nginx server
+1. Doctrine ORM
+1. FOSRestBundle
+1. Lexik JWT Authentication Bundle
+1. JWT Refresh Token Bundle
+1. Nelmio Cors Bundle
+1. PHPUnit
+1. NelmioApiDocBundle
+
+## Environment 
+1. Linux Debian 9
+1. Nginx with PHP 7.4
+1. PHPStorm
+1. MariaDB + MYSQL Workbench
---
+++ config/bundles.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+];
