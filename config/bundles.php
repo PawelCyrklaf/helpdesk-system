1209ca29986e44139bb1a1566ef3cca472dab260
Paweł Cyrklaf <pawel.cyrklaf@gmail.com>
2020-04-25 21:47:52 -0400

    Add twig and asset for nelmio api doc

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
