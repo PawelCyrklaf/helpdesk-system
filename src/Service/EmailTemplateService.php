9a7f7b67177537877e0731a3762bb370942a8b88
Paweł Cyrklaf <pawel.cyrklaf@gmail.com>
2020-05-01 19:21:55 -0400

    Add EmailTemplateService for email template, MailService for email send

--- src/Service/EmailTemplateService.php
+++ src/Service/EmailTemplateService.php
@@ -1,10 +1,29 @@
 <?php
 
-
 namespace App\Service;
 
+use App\Entity\Ticket;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Mime\Email;
 
 class EmailTemplateService
 {
+    public const NEW_TICKET_TEMPLATE = 0;
 
+    private ContainerInterface $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function getNewTicketEmailTemplate(Ticket $ticket): Email
+    {
+        $email = $ticket->getAuthor()->getEmail();
+        return (new Email())
+            ->from($this->container->getParameter('helpdesk_email'))
+            ->to($email)
+            ->subject(sprintf('Your ticket with id %d has been added', $ticket->getId()))
+            ->text('Your ticket has been added and waiting for answer!');
+    }
 }
--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -1,10 +1,32 @@
 <?php
 
-
 namespace App\Service;
 
+use App\Entity\Ticket;
+use Symfony\Component\Mailer\MailerInterface;
 
 class MailService
 {
+    private MailerInterface $mailer;
+    private EmailTemplateService $emailTemplateService;
 
+    public function __construct(
+        MailerInterface $mailer,
+        EmailTemplateService $emailTemplateService
+    )
+    {
+        $this->mailer = $mailer;
+        $this->emailTemplateService = $emailTemplateService;
+    }
+
+    public function send(Ticket $ticket, int $emailTemplate)
+    {
+        switch ($emailTemplate) {
+            case EmailTemplateService::NEW_TICKET_TEMPLATE:
+                $this->mailer->send($this->emailTemplateService->getNewTicketEmailTemplate($ticket));
+                break;
+            default:
+                return false;
+        }
+    }
 }
