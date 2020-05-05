2c3b975acb93e1e18b3107117ac9e855d02dfb14
Paweł Cyrklaf <pawel.cyrklaf@gmail.com>
2020-05-05 13:02:33 -0400

    Add reply service and controller

---
+++ src/Controller/ReplyController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ticket;
+use App\Service\ReplyService;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class ReplyController extends AbstractFOSRestController
+{
+    private ReplyService $replyService;
+    private EventDispatcherInterface $dispatcher;
+
+    public function __construct(
+        ReplyService $replyService,
+        EventDispatcherInterface $dispatcher
+    )
+    {
+        $this->replyService = $replyService;
+        $this->dispatcher = $dispatcher;
+    }
+
+    /**
+     * @Rest\Post("/ticket/{id}/reply")
+     * @param Request $request
+     * @param Ticket $ticket
+     * @return View
+     */
+    public function add(Request $request, Ticket $ticket)
+    {
+        $this->denyAccessUnlessGranted('TICKET_ADD_REPLY', $ticket);
+        if ($request->getContent() == null) {
+            throw new BadRequestHttpException('Request body cannot be null');
+        }
+
+        $replyData = json_decode($request->getContent(), true);
+        $user = $this->getUser();
+        $result = $this->replyService->add($replyData, $user);
+
+        if ($result instanceof Reply) {
+
+            return $this->view(['reply_id' => $result->getId()], Response::HTTP_OK);
+        }
+        return $this->view(['error' => $result], Response::HTTP_BAD_REQUEST);
+
+    }
+
+    /**
+     * @Rest\Put("/reply/{id}")
+     * @param Request $request
+     * @param Reply $reply
+     * @return View
+     */
+    public function update(Request $request, Reply $reply)
+    {
+        $this->denyAccessUnlessGranted('REPLY_EDIT', $reply);
+        if ($request->getContent() == null) {
+            throw new BadRequestHttpException('Request body cannot be null');
+        }
+
+        $replyData = json_decode($request->getContent(), true);
+        $result = $this->replyService->update($replyData, $reply);
+
+        if ($result) {
+            return $this->view([], Response::HTTP_OK);
+        }
+        return $this->view(['error' => $result], Response::HTTP_BAD_REQUEST);
+    }
+}
--- src/Service/ReplyService.php
+++ src/Service/ReplyService.php
@@ -1,10 +1,47 @@
 <?php
 
-
 namespace App\Service;
 
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ReplyService
 {
+    private ReplyRepository $replyRepository;
+    private ValidatorInterface $validator;
 
+    public function __construct(
+        ReplyRepository $replyRepository,
+        ValidatorInterface $validator
+    )
+    {
+        $this->replyRepository = $replyRepository;
+        $this->validator = $validator;
+    }
+
+    public function add(array $replyData, $author)
+    {
+        $message = $replyData['message'];
+
+        $reply = new Reply();
+        $reply->setMessage($message);
+        $reply->setAuthor($author);
+
+        $errors = $this->validator->validate($reply);
+        if (count($errors) > 0) {
+            return (string)$errors;
+        }
+
+        $this->replyRepository->save($reply);
+        return $reply;
+    }
+
+    public function update(array $replyData, Reply $reply): Reply
+    {
+        $message = $replyData['message'];
+
+        $reply->setMessage($message);
+        $this->replyRepository->update();
+
+        return $reply;
+    }
 }
