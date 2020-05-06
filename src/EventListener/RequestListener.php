6794746b2770044b86803874e906894b0ffb6866
Paweł Cyrklaf <pawel.cyrklaf@gmail.com>
2020-05-06 15:09:19 +0200

    Add RequestListener to check request body

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -1,10 +1,23 @@
 <?php
 
-
 namespace App\EventListener;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class RequestListener
 {
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $method = $event->getRequest()->getMethod();
+        $content = $event->getRequest()->getContent();
 
+        if ($method == Request::METHOD_POST || $method == Request::METHOD_PUT) {
+            if ($content == null || empty($content) || isset($content)) {
+                throw new BadRequestHttpException('Request body cannot be null');
+            }
+        }
+        return true;
+    }
 }
