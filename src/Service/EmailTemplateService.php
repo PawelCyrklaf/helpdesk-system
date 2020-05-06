6552edd9d8e9b223797c848bb344cb2198256067
Paweł Cyrklaf <pawel.cyrklaf@gmail.com>
2020-05-06 13:42:06 +0200

    Add new email template

--- src/Service/EmailTemplateService.php
+++ src/Service/EmailTemplateService.php
@@ -10,6 +10,7 @@ class EmailTemplateService
 {
     public const NEW_TICKET_TEMPLATE = 0;
     public const TICKET_CLOSED_TEMPLATE = 1;
+    public const TICKET_REPLY_TEMPLATE = 2;
 
     private ContainerInterface $container;
 
@@ -39,6 +40,19 @@ class EmailTemplateService
             ->to($email)
             ->subject(sprintf('Your ticket with id %d has been closed', $ticket->getId()))
             ->htmlTemplate('email/closed-ticket.html.twig')
+            ->context(array(
+                'ticket' => $ticket
+            ));
+    }
+
+    public function getNewReplyEmailTemplate(Ticket $ticket): TemplatedEmail
+    {
+        $email = $ticket->getAuthor()->getEmail();
+        return (new TemplatedEmail())
+            ->from($this->container->getParameter('helpdesk_email'))
+            ->to($email)
+            ->subject(sprintf('Nwq reply to your ticket with id %d', $ticket->getId()))
+            ->htmlTemplate('email/ticket-reply.html.twig')
             ->context(array(
                 'ticket' => $ticket
             ));
