7c92b6d47e0e410c446e65b08f820a6188ebfab6
Paweł Cyrklaf <pawel.cyrklaf@gmail.com>
2020-05-08 21:22:31 +0200

    Add partial implementation of controller and service

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,7 +6,9 @@ use App\Entity\User;
 use App\Service\UserService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends AbstractFOSRestController
 {
@@ -20,41 +22,64 @@ class UserController extends AbstractFOSRestController
     /**
      * @Rest\Post("/user")
      * @param Request $request
+     * @return View
      */
     public function add(Request $request)
     {
+        $result = $this->userService->add($request);
+        if ($result instanceof User) {
+            return $this->view(['user_id' => $result->getId()], Response::HTTP_OK);
+        }
+        return $this->view(['error' => $result], Response::HTTP_BAD_REQUEST);
     }
 
     /**
      * @Rest\Put("/user/{id}")
+     * @param Request $request
      * @param User $user
+     * @return View
      */
-    public function update(User $user)
+    public function update(Request $request, User $user)
     {
+        $result = $this->userService->update($request, $user);
+        if ($result) {
+            return $this->view([], Response::HTTP_OK);
+        }
+        return $this->view(['error' => $result], Response::HTTP_BAD_REQUEST);
     }
 
     /**
      * @Rest\Delete("/user/{id}")
      * @param User $user
+     * @return bool|View
      */
     public function remove(User $user)
     {
+        $result = $this->userService->remove($user);
+        if ($result) {
+            return $this->view([], Response::HTTP_NO_CONTENT);
+        }
+        return false;
     }
 
     /**
      * @Rest\Get("/user/{id}")
      * @param User $user
+     * @return View
      */
     public function details(User $user)
     {
+        return $this->view($user, Response::HTTP_OK);
     }
 
     /**
      * @Rest\Get("/users")
      * @param Request $request
+     * @return View
      */
     public function list(Request $request)
     {
+        $users = $this->userService->getUsers($request);
+        return $this->view($users, Response::HTTP_OK);
     }
-
 }
--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -4,19 +4,54 @@ namespace App\Service;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserService
 {
     private UserRepository $userRepository;
+    private ValidatorInterface $validator;
+    private PaginatorInterface $paginator;
+    private UserPasswordEncoderInterface $encoder;
 
-    public function __construct(UserRepository $userRepository)
+    public function __construct(
+        UserRepository $userRepository,
+        ValidatorInterface $validator,
+        PaginatorInterface $paginator,
+        UserPasswordEncoderInterface $encoder)
     {
         $this->userRepository = $userRepository;
+        $this->validator = $validator;
+        $this->paginator = $paginator;
+        $this->encoder = $encoder;
     }
 
     public function add(Request $request)
     {
+        $userData = json_decode($request->getContent(), true);
+        $name = $userData['name'];
+        $surname = $userData['surname'];
+        $email = $userData['email'];
+        $password = $userData['password'];
+
+        $user = new User();
+        $user->setName($name);
+        $user->setSurname($surname);
+        $user->setEmail($email);
+        $encodedPassword = $this->encoder->encodePassword($user, $password);
+        $user->setPassword($encodedPassword);
+        $user->setRoles(array('ROLE_ADMIN'));
+
+        $errors = $this->validator->validate($user);
+        if (count($errors) > 0) {
+            throw new BadRequestHttpException($errors);
+        }
+
+        $this->userRepository->save($user);
+        return $user;
     }
 
     public function update(Request $request, User $user): User
@@ -29,7 +64,7 @@ class UserService
         return true;
     }
 
-    public function list(Request $request): iterable
+    public function getUsers(Request $request): iterable
     {
     }
 }
