e13aae7cfed47bd37306304d0df4e92071782606
Pawel Cyrklaf <pawel.cyrklaf@gmail.com>
2020-06-07 16:24:02 +0200

    Add User entity to DoctrineListener

--- src/EventListener/DoctrineListener.php
+++ src/EventListener/DoctrineListener.php
@@ -4,6 +4,7 @@ namespace App\EventListener;
 
 use App\Entity\Reply;
 use App\Entity\Ticket;
+use App\Entity\User;
 use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Security\Core\Security;
@@ -37,6 +38,8 @@ class DoctrineListener
             $this->logger->info(sprintf('Ticket with id %d has been ' . $method . ' for user %s', $entity->getId(), $this->security->getUser()->getUsername()));
         } elseif ($entity instanceof Reply) {
             $this->logger->info(sprintf('Reply with id %d has been ' . $method . ' for user with id %d', $entity->getId(), $this->security->getUser()->getUsername()));
+        } elseif ($entity instanceof User) {
+            $this->logger->info(sprintf('User with id %d has been ' . $method, $entity->getId()));
         }
     }
 }
