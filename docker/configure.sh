a69c4c0b75ee5d6261bfb874ea851ae00ee77edc
Pawel Cyrklaf <pawel.cyrklaf@gmail.com>
2020-09-18 09:51:31 +0200

    Add elasticsearch to docker-compose

--- docker/configure.sh
+++ docker/configure.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker-compose exec php php bin/console doctrine:schema:update --force
 docker-compose exec php php bin/console doctrine:fixtures:load --no-interaction
+docker-compose exec php php bin/console fos:elastica:populate
