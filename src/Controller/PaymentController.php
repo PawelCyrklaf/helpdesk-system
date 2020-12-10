bf8d9eb7e868bbe43fff996c108eacfced077a6b
Pawel Cyrklaf <pawel.cyrklaf@gmail.com>
2020-12-10 18:55:08 +0100

    Change Payment and Subscription controller

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Service\PaymentService;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Swagger\Annotations as SWG;
 
-class PaymentController extends AbstractController
+class PaymentController extends AbstractFOSRestController
 {
+    private PaymentService $paymentService;
 
+    public function __construct(PaymentService $paymentService)
+    {
+        $this->paymentService = $paymentService;
+    }
 }
--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Service\SubscriptionService;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Swagger\Annotations as SWG;
 
-class SubscriptionController extends AbstractController
+class SubscriptionController extends AbstractFOSRestController
 {
+    private SubscriptionService $subscriptionService;
 
+    public function __construct(SubscriptionService $subscriptionService)
+    {
+        $this->subscriptionService = $subscriptionService;
+    }
 }
