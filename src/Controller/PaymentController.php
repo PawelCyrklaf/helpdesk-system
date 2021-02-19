b9c615a606e016c65ecd2685ff56f25fa46f33a6
Pawel Cyrklaf <pawel.cyrklaf@gmail.com>
2021-02-19 18:53:34 +0100

    Add controllers skeleton

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -6,6 +6,7 @@ use App\Service\PaymentService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Request;
 
 class PaymentController extends AbstractFOSRestController
 {
@@ -14,5 +15,30 @@ class PaymentController extends AbstractFOSRestController
     public function __construct(PaymentService $paymentService)
     {
         $this->paymentService = $paymentService;
+    }
+
+    public function add(Request $request, Payment $payment)
+    {
+
+    }
+
+    public function update(Request $request, Payment $payment)
+    {
+
+    }
+
+    public function remove(Payment $payment)
+    {
+
+    }
+
+    public function details(Payment $payment)
+    {
+
+    }
+
+    public function list(Request $request)
+    {
+
     }
 }
--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -6,6 +6,7 @@ use App\Service\SubscriptionService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Swagger\Annotations as SWG;
+use Symfony\Component\HttpFoundation\Request;
 
 class SubscriptionController extends AbstractFOSRestController
 {
@@ -14,5 +15,30 @@ class SubscriptionController extends AbstractFOSRestController
     public function __construct(SubscriptionService $subscriptionService)
     {
         $this->subscriptionService = $subscriptionService;
+    }
+
+    public function add(Request $request, Subscription $subscription)
+    {
+
+    }
+
+    public function update(Request $request, Subscription $subscription)
+    {
+
+    }
+
+    public function remove(Subscription $subscription)
+    {
+
+    }
+
+    public function details(Subscription $subscription)
+    {
+
+    }
+
+    public function list(Request $request)
+    {
+
     }
 }
